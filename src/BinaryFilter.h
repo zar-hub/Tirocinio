7ba3c188287dd6fb07362844cfd22ff647f055c6
Alberto Mazzarotto <albertomazzarotto+pythonLab@gmail.com>
2023-11-24 13:25:37 +0100

    working save

--- src/BinaryFilter.h
+++ src/BinaryFilter.h
@@ -11,28 +11,49 @@ class BinaryFilter
     Double_t areaFailed;
     Double_t efficiency;
     Double_t efficencyError;
+    Bool_t (*filter)() = nullptr;
+    Bool_t isLeaf = false;
 
 public:
-    SmartGraph *passed;
-    SmartGraph *failed;
+    SmartGraph *histo;             // this is the histo of the PARENT!
+    BinaryFilter *left = nullptr;  // left child
+    BinaryFilter *right = nullptr; // right child
 
-    BinaryFilter(const string &name, FitPrototype &passedPrototype, FitPrototype &failedPrototype, TVirtualPad *padPassed, TVirtualPad *padFailed) : name(name)
+    BinaryFilter(const string &name, FitPrototype &fitPrototype, TVirtualPad *nodePad) : name(name)
     {
-        passed = new SmartGraph((name + "Passed").c_str(), passedPrototype, padPassed);
-        failed = new SmartGraph((name + "Failed").c_str(), failedPrototype, padFailed);
-        passed->setStyle(true);
-        failed->setStyle(false);
+        // < Filter NOT given />
+        // the object acts like a leaf
+        cout << "BinaryFilter leaf : " << name << " created" << endl;
+        isLeaf = true;
+        histo = new SmartGraph(name.c_str(), fitPrototype, nodePad);
     }
 
-    void FitAndDraw()
+    BinaryFilter(const string &name, Bool_t (*filter)(), FitPrototype &passedPrototype, FitPrototype &failedPrototype, TVirtualPad *nodePad) : name(name), filter(filter)
     {
-        // the two operations must be done both
-        passed->FitAndDraw();
-        failed->FitAndDraw();
-        areaPassed = passed->getSignalIntegral();
-        areaFailed = failed->getSignalIntegral();
+        // < Filter IS given />
+        // the object acts like a node
+        cout << "BinaryFilter: " << name << " created" << endl;
+        nodePad->Divide(2, 1);
+        left = new BinaryFilter(name + "Passed", passedPrototype, nodePad->GetPad(1));
+        right = new BinaryFilter(name + "Failed", failedPrototype, nodePad->GetPad(2));
+    }
+
+    Double_t FitAndDraw()
+    {
+        // the leaf just renders the histo
+        if (isLeaf)
+        {
+            histo->FitAndDraw();
+            return histo->getSignalIntegral();
+        }
+
+        left->FitAndDraw();
+        right->FitAndDraw();
+        areaPassed = left->histo->getSignalIntegral();
+        areaFailed = right->histo->getSignalIntegral();
         efficiency = areaPassed / (areaPassed + areaFailed);
         efficencyError = sqrt(efficiency * (1 - efficiency) / (areaPassed + areaFailed));
+        return areaPassed + areaFailed;
     }
 
     void printInfo()
@@ -41,11 +62,11 @@ public:
         icout.identUp();
         icout << "Passed: " << endl;
         icout.identUp();
-        passed->printInfo();
+        left->histo->printInfo();
         icout.identDown();
         icout << "Failed: " << endl;
         icout.identUp();
-        failed->printInfo();
+        right->histo->printInfo();
         icout.identDown();
         // efficiency considerations
         icout << "Efficiency: " << efficiency << "\u00b1" << efficencyError << endl;
--- testBinaryFIlter.C
+++ testBinaryFIlter.C
@@ -11,15 +11,20 @@
 #include <TSystem.h>
 #include <TF1.h>
 
+Bool_t testCondition(){
+    return true;
+}
+
 void analisi::Loop()
 {
 
     // create canvas and SmartGraphs
     auto canvas = new TCanvas("mycanvas", "mycanvas", 0, 0, 1600, 1000);
-    canvas->Divide(2, 2);
-    auto grBarrel = new BinaryFilter("barrel", rettaBigausPrototype, expBigausPrototype, canvas->GetPad(1), canvas->GetPad(2));
-    auto grEndcaps = new BinaryFilter("endcaps", rettaBigausPrototype, expBigausPrototype, canvas->GetPad(3), canvas->GetPad(4));
-
+    canvas->Divide(1, 1); // hack to create a single pad
+    TVirtualPad *rootPad = canvas->GetPad(1);
+    // leaf element
+    auto grBarrel = new BinaryFilter("Barrel", expBigausPrototype, rootPad);
+    
     // style
     setGlobalStyle();
     // health check
@@ -46,24 +51,28 @@ void analisi::Loop()
         if (GoodWP80(1))
         {
             if (eta[1] <= 1.4442)
-                grBarrel->passed->Fill(mee);
-            else if ((abs(eta[1]) >= 1.566) && (abs(eta[1]) <= 2.5))
-                grEndcaps->passed->Fill(mee);
+                grBarrel->histo->Fill(mee);
+            //else if ((abs(eta[1]) >= 1.566) && (abs(eta[1]) <= 2.5))
+            // grEndcaps->passed->Fill(mee);
         }
         else
         {
             if (eta[1] <= 1.4442)
-                grBarrel->failed->Fill(mee);
-            else if ((abs(eta[1]) >= 1.566) && (abs(eta[1]) <= 2.5))
-                grEndcaps->failed->Fill(mee);
+                grBarrel->histo->Fill(mee);
+           // else if ((abs(eta[1]) >= 1.566) && (abs(eta[1]) <= 2.5))
+            // grEndcaps->failed->Fill(mee);
         }
     }
 
     // Draw the graphs
     grBarrel->FitAndDraw();
-    grEndcaps->FitAndDraw();
+    // grEndcaps->FitAndDraw();
 
     printHeader("Fit results");
-    grBarrel->printInfo();
-    grEndcaps->printInfo();
+    
+    // test draw
+    // grBarrel->left->histo->Draw();
+
+    //grBarrel->printInfo();
+    // grEndcaps->printInfo();
 }
